f5e8be0def7b47ed87bb9e6d8c492aa2d3ea0c43
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-07-29 21:54:22 -0300

    exercicio 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: '',
+    }
+    
+    this.apiDog = this.apiDog.bind(this);
+  }
+
+  apiDog() {
+    const api = 'https://dog.ceo/api/breeds/image/random';
+    fetch(api).then(data => data.json())
+    .then((response) => {
+      this.setState({
+        data: response,
+      })
+    })
+    return <img src={this.state.picture} alt={this.state.picture}></img>
+  }
+
+  componentDidMount() {
+    this.apiDog();
+  }
+
+  splitDogBreed(dogBreed) {
+    const result = dogBreed.split('/');
+    return alert(result[4]);
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('photo', this.state.data.message);
+    console.log(this.splitDogBreed(this.state.data.message))
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if(nextState.data.message.includes('terrier') === true) return false;
+    return true;
+  }
+
+  render() {
+    const { data } = this.state;
+    const loading = <p>loading...</p>
+    if(data === '') return loading
+    return (
+      <div>
+        <img src={data.message} alt={data.message}></img>
+        <button type='button' onClick={ this.apiDog }>new dog picture</button>
+      </div>
+    )
+  }
 }
 
 export default App;
